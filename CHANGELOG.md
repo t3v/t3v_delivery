b818578384ccd9253771cf61977ef428fa5023aa
Maik Kempe <mkempe@bitaculous.com>
2018-06-06 22:23:50 +0200

    [➦] Merged `develop` branch.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,14 @@ Changelog
 
 Notable changes will be documented in this file. The project adheres to [Semantic Versioning].
 
+Unreleased
+----------
+
+* Improved `RsyncCommandController`
+* Refactoring and Type Hinting and improved tests
+* Updated AppVeyor configuration
+* Cleaned up
+
 1.0.0
 -----
 
--- Classes/Command/RsyncCommandController.php
+++ Classes/Command/RsyncCommandController.php
@@ -15,6 +15,13 @@ use T3v\T3vCore\Command\AbstractCommandController;
  */
 class RsyncCommandController extends AbstractCommandController {
   /**
+   * If the extension is enabled.
+   *
+   * @var bool
+   */
+  protected $enabled = false;
+
+  /**
    * The configuration.
    *
    * @var \PHLAK\Config
@@ -31,9 +38,7 @@ class RsyncCommandController extends AbstractCommandController {
       $extensionConfiguration = @unserialize($extensionConfiguration);
     }
 
-    if (is_array($extensionConfiguration)) {
-      $this->extensionConfiguration = $extensionConfiguration;
-
+    if (is_array($extensionConfiguration) && !empty($extensionConfiguration)) {
       $enabled = (boolean) $extensionConfiguration['enabled'];
 
       if ($enabled) {
@@ -47,20 +52,20 @@ class RsyncCommandController extends AbstractCommandController {
   /**
    * The sync command.
    *
-   * @param boolean $verbose The optional verbosity, defaults to `false`
+   * @param bool $verbose The optional verbosity, defaults to `false`
    */
-  public function syncCommand($verbose = false) {
-    $verbose = (boolean) $verbose;
-
+  public function syncCommand(bool $verbose = false) {
     if ($this->enabled) {
       $this->log('Syncing...', 'info', $verbose);
 
       $buckets = $this->configuration->get('buckets');
 
-      if (is_array($buckets)) {
+      if (is_array($buckets) && !empty($buckets)) {
         foreach ($buckets as $name => $configuration) {
           $this->syncBucket($name, $configuration, $verbose);
         }
+      } else {
+        $this->log('No buckets defined.', 'error', $verbose);
       }
 
       $this->log('Syncing completed.', 'ok', $verbose);
@@ -70,86 +75,86 @@ class RsyncCommandController extends AbstractCommandController {
   /**
    * Syncs a bucket.
    *
-   * @param array $name The bucket name
+   * @param string $name The bucket name
    * @param array $configurtion The bucket configuration
-   * @param boolean $verbose The optional verbosity, defaults to `false`
+   * @param bool $verbose The optional verbosity, defaults to `false`
    */
-  protected function syncBucket($name, $configuration, $verbose = false) {
-    $name    = (string) $name;
-    $sources = $configuration['sources'];
-    $verbose = (boolean) $verbose;
-
+  protected function syncBucket(string $name, array $configuration, bool $verbose = false) {
     $this->log("Syncing `${name}` bucket...", 'info', $verbose);
 
-    try {
-      $fileLock = new FileLock($this->getLockFilePath($name));
+    $sources = $configuration['sources'];
 
-      $fileLock->acquire();
+    if (is_array($sources) && !empty($sources)) {
+      try {
+        $fileLock = new FileLock($this->getLockFilePath($name));
+        $fileLock->acquire();
 
-      foreach ($sources as $source) {
-        $path   = (string) $source['path'];
-        $target = (string) $configuration['target'];
+        foreach ($sources as $source) {
+          $path   = (string) $source['path'];
+          $target = (string) $configuration['target'];
 
-        if (!empty($path) && !empty($target)) {
-          $origin = PATH_site . $path;
+          if (!empty($path) && !empty($target)) {
+            $origin = PATH_site . $path;
 
-          if (is_dir($origin)) {
-            $exclude          = $source['exclude']            ?? [];
-            $recursive        = $source['recursive']          ?? true;
-            $followSymlinks   = $source['follow_symlinks']    ?? true;
-            $deleteFromTarget = $source['delete_from_target'] ?? false;
-            $archive          = $source['archive']            ?? false;
-            $dryRun           = $source['dry_run']            ?? false;
+            if (is_dir($origin)) {
+              $exclude          = $source['exclude']            ?? [];
+              $recursive        = $source['recursive']          ?? true;
+              $followSymlinks   = $source['follow_symlinks']    ?? true;
+              $deleteFromTarget = $source['delete_from_target'] ?? false;
+              $archive          = $source['archive']            ?? false;
+              $dryRun           = $source['dry_run']            ?? false;
 
-            $rsync = new Rsync;
-            $rsync->setExclude($exclude);
-            $rsync->setRecursive($recursive);
-            $rsync->setFollowSymlinks($followSymlinks);
-            $rsync->setDeleteFromTarget($deleteFromTarget);
-            $rsync->setArchive($archive);
-            $rsync->setDryRun($dryRun);
-            $rsync->setVerbose($verbose);
+              $rsync = new Rsync;
+              $rsync->setExclude($exclude);
+              $rsync->setRecursive($recursive);
+              $rsync->setFollowSymlinks($followSymlinks);
+              $rsync->setDeleteFromTarget($deleteFromTarget);
+              $rsync->setArchive($archive);
+              $rsync->setDryRun($dryRun);
+              $rsync->setVerbose($verbose);
 
-            if (is_array($configuration['ssh'])) {
-              $rsync->setSshOptions($configuration['ssh']);
+              if (is_array($configuration['ssh'])) {
+                $rsync->setSshOptions($configuration['ssh']);
+              }
+
+              $relativePath = $this->getRelativePath($path);
+
+              if ($relativePath) {
+                $target = $target . $relativePath;
+              }
+
+              $rsync->sync($origin, $target);
+            } else {
+              $this->log("Directory: `${origin}` not found.", 'error', $verbose);
             }
-
-            $relativePath = $this->getRelativePath($path);
-
-            if ($relativePath) {
-              $target = $target . $relativePath;
-            }
-
-            $rsync->sync($origin, $target);
-          } else {
-            $this->log("Directory: `${origin}` not found.", 'error', $verbose);
           }
         }
+
+        $fileLock->release();
+
+        $this->log("The bucket called `${name}` has been synced.", 'ok', $verbose);
+      } catch (\TH\Lock\Exception $exception) {
+        $this->log("The bucket called `${name}` is currently being synced.", 'warning', $verbose);
       }
-
-      $fileLock->release();
-
-      $this->log("The bucket called `${name}` has been synced.", 'ok', $verbose);
-    } catch (\TH\Lock\Exception $exception) {
-      $this->log("The bucket called `${name}` is currently being synced.", 'warning', $verbose);
+    } else {
+      $this->log("The bucket called `${name}` has no sources defined.", 'error', $verbose);
     }
   }
 
   /**
    * Loads the configuration.
    *
-   * @param string $configurationFile The configuration file, related to the web site root directory
+   * @param string $configurationFile The configuration file, relative to the web site root directory
+   * @throws \Exception
   */
-  protected function loadConfiguration($configurationFile) {
-    $configurationFile = (string) $configurationFile;
-
+  protected function loadConfiguration(string $configurationFile) {
     if ($configurationFile) {
       $configurationFile = PATH_site . $configurationFile;
 
       if (@file_exists($configurationFile)) {
         $this->configuration = new Config\Config($configurationFile);
       } else {
-        throw new \Exception("Configuration file: `${configurationFile}` not found.");
+        throw new \Exception("Configuration file `${configurationFile}` doesn't exist.");
       }
     }
   }
@@ -160,8 +165,7 @@ class RsyncCommandController extends AbstractCommandController {
    * @param string $identifier The identifier
    * @return string The lock file path
   */
-  protected function getLockFilePath($identifier) {
-    $identifier   = (string) $identifier;
+  protected function getLockFilePath(string $identifier): string {
     $tempFolder   = PATH_site . 'typo3temp';
     $lockFilePath = "${tempFolder}/t3v_delivery-${identifier}.lock";
 
@@ -174,8 +178,7 @@ class RsyncCommandController extends AbstractCommandController {
    * @param string $path The path
    * @return string|null The relative path or null
   */
-  protected function getRelativePath($path) {
-    $path         = (string) $path;
+  protected function getRelativePath(string $path) {
     $segments     = explode('/', $path);
     $relativePath = null;
 
